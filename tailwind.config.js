1f7f8f93a6ca4a14aabeca43f278c7fb44b1c9fa
Vinay Naik <vinaynaik1214@gmail.com>
2024-10-10 06:36:34 +0530

    Added new fontface

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sofia: ["Sofia Pro", "sans-serif"], // Regular (default)
+        "sofia-italic": ["Sofia Pro Italic", "sans-serif"], // Italic
+        "sofia-extralight": ["Sofia Pro ExtraLight", "sans-serif"], // ExtraLight
+        "sofia-ultralight": ["Sofia Pro UltraLight", "sans-serif"], // UltraLight
+        "sofia-extralight-italic": [
+          "Sofia Pro ExtraLight Italic",
+          "sans-serif",
+        ], // ExtraLight Italic
+      },
+    },
   },
   plugins: [],
 };
