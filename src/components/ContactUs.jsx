762b8c6db08ab4137a0299a1513a63919f521c55
Vinay Naik <vinaynaik1214@gmail.com>
2024-10-13 00:21:18 +0530

    feat : Added contact us, footer, pricing section component

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Btn from './ui/btn'
+
+function ContactUs() {
+  return (
+
+    <section className="bg-white">
+      <div className="py-8 lg:py-8 px-4 mx-auto max-w-screen-md">
+        <h2 className="mb-4 text-4xl tracking-tight font-sofia-bold text-center text-gray-900">
+          Get in touch with us
+        </h2>
+        <p className="mb-8 lg:mb-16 font-sofia-light text-center text-gray-500  sm:text-md">
+          Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
+        </p>
+        <form action="#" className="space-y-6">
+          <div>
+            <label htmlFor="email" className="block mb-2 text-sm font-sofia-medium text-gray-900">
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+              placeholder="name@wavemeet.com"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="subject" className="block mb-2 text-sm font-sofia-medium text-gray-900">
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 "
+              placeholder="Let us know how we can help you"
+              required
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="message" className="block mb-2 text-md font-sofia-medium text-gray-900">
+              Your message
+            </label>
+            <textarea
+              id="message"
+              rows="6"
+              className="block p-2.5 w-full text-sm resize-none text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+              placeholder="Leave a comment..."
+            ></textarea>
+          </div>
+          <Btn text="Send message" />
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default ContactUs
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PricingSection from './ui/pricingsection'
+
+
+function Pricing() {
+  const pricingPlans = [
+    {
+      plan: 'Free',
+      price: "0",
+      features: ['Up to 50 participants per meeting', 'Meeting duration: up to 30 minutes', 'Screen Sharing'],
+      isPopular: false
+    },
+    {
+      plan: 'Plus',
+      price: 199,
+      features: ['Up to 100 participants per meeting', 'Meeting duration: up to 2 hours', 'Full HD (1080p) video quality', 'Screen Sharing'],
+      isPopular: true
+    },
+    {
+      plan: 'Pro',
+      price: 799,
+      features: ['Up to 250 participants per meeting', 'Meeting duration: Unlimited', 'Full HD (1080p) video quality', 'Advanced Multi-screen sharing', '24/7 premium customer support'],
+      isPopular: false
+    }
+  ];
+  return (
+    <PricingSection pricingPlans={pricingPlans} />
+  )
+}
+
+export default Pricing;
