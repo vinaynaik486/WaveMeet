453c4df20e9e5ea133107260912f25131f0469f6
Vinay Naik <vinaynaik1214@gmail.com>
2024-10-12 18:17:01 +0530

    Made Mobile Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,21 +16,22 @@ function Hero() {
   }, []);
 
   return (
-    <div ref={heroRef} className="font-sofia mx-8 flex flex-col items-center">
-      <p className="reveal-text font-bold text-4xl mt-20 text-[#222222] text-center">
-        Immersive Meetings, Anywhere
+    <div ref={heroRef} className="font-sofia mx-5 md:mx-28 flex flex-col items-center text-center">
+      <p className="reveal-text font-bold text-3xl md:text-5xl mt-10 md:mt-20 text-[#222222]">
+        Create Meetings with ease
       </p>
-      <p className="reveal-text font-sofia-ultralight mt-5 text-[#666666] text-center max-w-lg">
+      <p className="reveal-text font-sofia-ultralight text-sm md:text-md mt-3 md:mt-5 text-[#666666] max-w-md md:max-w-lg">
         Effortlessly connect with high-quality video and audio. Experience engaging meetings that bring your team closer.
       </p>
-      <div className="flex mt-6 justify-center gap-4 reveal-text">
+      <div className="flex mt-4 md:mt-6 justify-center gap-2 md:gap-4 reveal-text">
         <Btn ref={heroRef} text={"Create Meeting"} />
-        <button className='border border-gray-400 rounded-md p-3 hover:scale-105 ease-in-out duration-300 '>Register Now</button>
+        <button className='border border-gray-400 rounded-md p-2 md:p-3 hover:scale-105 ease-in-out duration-300'>Register Now</button>
       </div>
       <div className='reveal-text'>
-        <img src={HeroCover} alt="" className='mt-16 w-[60rem] mb-16 rounded-xl shadow-[0_0_75px_-30px_rgba(0,0,0,0.75)]' />
+        <img src={HeroCover} alt="" className='mt-10 md:mt-16 w-full max-w-[30rem] md:max-w-[60rem] mb-10 md:mb-16 rounded-xl shadow-[0_0_75px_-30px_rgba(0,0,0,0.75)]' />
       </div>
     </div>
+
   );
 }
 
