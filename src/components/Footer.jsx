ef24f39781f059749c3223b5c2775fe4cab90c62
Vinay Naik <vinaynaik1214@gmail.com>
2024-10-15 02:09:56 +0530

    feat : did the page resposive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
-import { HashLink } from 'react-router-hash-link';
 import websiteLogo from '../assets/logo/logo.png';
 
 function Footer() {
   return (
     <footer className="w-full bg-white p-3">
-      <hr className="my-4 mx-16 border-gray-600" />
-      <div className="flex justify-between mx-20 items-center">
-        <div className="flex items-center font-sofia">
-          <img src={websiteLogo} alt="" className='h-10' />
-          <span className='text-xl font-semibold reveal-text'>WaveMeet</span>
+      <hr className="my-4 mx-4 sm:mx-8 md:mx-16 lg:mx-20 border-gray-600" />
+      <div className="flex flex-col lg:flex-row justify-between mx-4 sm:mx-8 md:mx-16 lg:mx-20 items-center space-y-4 lg:space-y-0">
+        <div className="flex items-center font-sofia cursor-pointer">
+          <img src={websiteLogo} alt="WaveMeet logo" className="h-8 sm:h-10" />
+          <span className="text-lg sm:text-xl font-semibold reveal-text ml-2">WaveMeet</span>
         </div>
-        <p>&copy;2024 WaveMeet. All Rights Reserved</p>
+        <p className="text-center lg:text-left">&copy;2024 WaveMeet. All Rights Reserved</p>
       </div>
     </footer>
   );
 }
+
 export default Footer;
