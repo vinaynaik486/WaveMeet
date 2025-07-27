7bb3894cf2513f50e85c50af420a1610c75a9cce
Vinay Naik <vinaynaik1214@gmail.com>
2025-07-27 11:42:00 +0530

    feat(backend): add custom winston logger utility

---
+++ src/backend/utils/logger.js
@@ -0,0 +1,17 @@
+import pino from 'pino';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const logger = pino({
+  level: isProduction ? 'silent' : 'info',
+  transport: isProduction ? undefined : {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      ignore: 'pid,hostname',
+      translateTime: 'SYS:standard',
+    },
+  },
+});
+
+export default logger;
